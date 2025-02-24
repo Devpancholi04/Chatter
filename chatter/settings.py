0c0a867c7f040e107dd631c6bbd3bafc80340de8
Dev <devpancholigt2004@gmail.com>
2025-02-24 19:55:41 +0530

    added the mysql database conf. to settings.py file

--- chatter/settings.py
+++ chatter/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'chatter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'chatter_app',
+        'USER' : 'root',
+        'PASSWORD' : 'DEV19163247',
+        "HOST" : 'localhost',
+        "PORT" : '3306'
     }
 }
 

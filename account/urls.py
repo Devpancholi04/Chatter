0f88722dbe7525b20e66977645bfb5e01ed82c72
Dev <devpancholigt2004@gmail.com>
2025-03-30 15:07:16 +0530

    updated the urls file in the accounts app where in the account_activation added = sign with the ref to transfer data securly without error

--- account/urls.py
+++ account/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('register/', views.registration_page, name="register"),
 
     path('login/', views.login_page, name="login"),
-    path('ref<str:user_id>/activate/tk=<str:email_token>/', views.activate_account, name="activate_account"),
+    path('ref=<str:user_id>/activate/tk=<str:email_token>/', views.activate_account, name="activate_account"),
     path('login/verifyotp/ref=<str:user_id>/id=<str:username>/', views.verify_otp, name="verify_otp"),
 
     path('login/resetpassword/', views.reset_password, name="reset_password"),

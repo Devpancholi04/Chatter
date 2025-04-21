83d60741c4345d9fc308446cb5cda9f8676ba38b
Dev <devpancholigt2004@gmail.com>
2025-04-22 02:50:16 +0530

    in the community app > tasks.py > update the notify_user function with as the message send to the user it should be updated in the community_recent_cache data to update without reloading the page and directly from the cache

--- community/tasks.py
+++ community/tasks.py
@@ -1,4 +1,4 @@
-from .models import Community, CommunityMember, CommunityMessage
+from .models import Community, CommunityMember, CommunityMessage,CommunityMessageReadReceipent
 
 from django.core.cache import cache
 from celery import shared_task
@@ -14,16 +14,43 @@ def notify_user(community_id, new_username):
         community = Community.objects.get(community_id = community_id)
         new_user = user.objects.get(username = new_username)
         message_time = datetime.now()
+        get_system = user.objects.get(username = 'System')
 
         system_message = f"Hello {new_user.first_name} {new_user.last_name},\nWelcome to {community.community_name}"
 
         message = CommunityMessage.objects.create(
             community = community,
-            sender = None,
+            sender = get_system,
             message = system_message,
         )
 
         member = CommunityMember.objects.filter(community = community).select_related('member')
+
+        community_chat_history_key = f"COMMUNITY-CHAT-CACHE:{community_id}"
+        list_messages = cache.get(community_chat_history_key, [])
+        
+        read_receiver = CommunityMessageReadReceipent.objects.filter(message=system_message, is_read = True).exclude(member__username = "System")
+        readby = [receiver.member.username for receiver in read_receiver]
+
+        list_messages.append({
+            'message_id' : message.message_id,
+            'community_id' : community_id,
+            'community_name' : community.community_name,
+            'sender_id' : message.sender.username,
+            'received_id' : {
+                'all_message_list' : [
+                    members.member.username for members in community.community_members.all()
+                    if members.member and members.member.username != "System"
+                ],
+            },
+            'sender_name' : f"{message.sender.first_name}",
+            'message' : system_message,
+            'is_send' : True,
+            'is_read' : CommunityMessageReadReceipent.objects.filter(message = system_message, member__username = "System", is_read = True).exists(),
+            'readed_by' : readby,
+            'date' : message.updated_at.strftime("%d-%m-%Y"),
+            'time' : message.updated_at.strftime("%I:%M %p"),
+        })
 
         for memb in member:
             if memb.member:
@@ -48,6 +75,7 @@ def notify_user(community_id, new_username):
                         })
 
                         cache.set(cache_key, get_cache, timeout=None)
-
+        cache.set(community_chat_history_key, list_messages, timeout=None)
+        
     except Exception as e:
         print(f"[celery task error] notify_user : {e}")
